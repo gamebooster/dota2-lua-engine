4c9c026dae49430ad5334f5f6d2ff8582291c169
Karl Skomski <karl@skomski.com>
2013-08-09 23:06:32 +0200

    Cleanup utils.h

--- dota2-esp/utils/utils.h
+++ dota2-esp/utils/utils.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 namespace utils {
   const unsigned char* FindPattern(const void* address, size_t size, const unsigned char* signature, const char* mask);
   const unsigned char* FindPattern(const char* module_name, const unsigned char* signature, const char* mask);
@@ -13,21 +12,18 @@ namespace utils {
   void Log(const char* format, ...);
   const std::wstring ConvertToWide(const char *text);
   const std::string ConvertToANSI(const wchar_t *text);
+  int ConvertANSIToUnicode(const char *ansi, wchar_t *unicode, int unicodeBufferSizeInBytes);
+  int ConvertUnicodeToANSI(const wchar_t *unicode, char *ansi, int ansiBufferSize);
+  std::string GetModuleDirectory();
 
-  inline void**& GetVtable( void* inst, size_t offset = 0 )
-  {
+  inline void**& GetVtable( void* inst, size_t offset = 0) {
     return *reinterpret_cast<void***>( (size_t)inst + offset );
   }
-  inline const void** GetVtable( const void* inst, size_t offset = 0 )
-  {
+  inline const void** GetVtable( const void* inst, size_t offset = 0) {
     return *reinterpret_cast<const void***>( (size_t)inst + offset );
   }
   template< typename Fn >
-  inline Fn GetVtableFunction( const void* inst, size_t index, size_t offset = 0 )
-  {
+  inline Fn GetVtableFunction( const void* inst, size_t index, size_t offset = 0) {
     return reinterpret_cast<Fn>( GetVtable( inst, offset )[ index ] );
   }
-  int ConvertANSIToUnicode(const char *ansi, wchar_t *unicode, int unicodeBufferSizeInBytes);
-  int ConvertUnicodeToANSI(const wchar_t *unicode, char *ansi, int ansiBufferSize);
-    std::string GetModuleDirectory();
 }
