e10b91bf6dc82c49743a377dde2a89c1b8ca29e1
Karl Skomski <karl@skomski.com>
2013-08-15 14:26:30 +0200

    Some teaks for axe_autoult.lua

--- scripts/axe_autoult.lua
+++ scripts/axe_autoult.lua
@@ -1,20 +1,22 @@
 local hook = nil
 local sleep = 0
+local local_hero = dota.GetLocalHero()
+local local_player = dota.GetLocalPlayer()
  
 function OnPaint()
   if sleep > 0 then
     sleep = sleep - 1
     return
   end
-  local players = dota.FindEntities(dota.kDotaPlayer)
-  local local_player = dota.GetLocalPlayer()
-  local local_hero = dota.GetLocalHero()
+
   local ult_ability = local_hero:GetAbilityByDisplayedIndex(3)
   local threshold = ult_ability:GetLevelSpecialValueFor("kill_threshold")
-  if threshold == 0 then
+  if threshold == 0 or ult_ability:CanBeExecuted() != -1 then
     return
   end
 
+  local players = dota.FindEntities(dota.kDotaPlayer)
+  
   for _,player in ipairs(players) do
     local hero = player:GetAssignedHero()
     if hero == nil then
@@ -23,8 +25,7 @@ function OnPaint()
     
     local health = hero:GetHealth()
     if hero:GetTeamIndex() ~= local_hero:GetTeamIndex() and
-       health <= threshold and health > 10 and
-       ult_ability:CanBeExecuted() == -1 and
+       health <= threshold and health > 50 and
        local_hero:IsEntityInRange(hero, ult_ability:GetCastRange()) then
       local_player:UseAbilityEntity(ult_ability, hero)
       sleep = 50
